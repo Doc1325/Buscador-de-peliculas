1bffab2f8c46212cfefb6c5d365d00e95d317f59
Doc1325 <57734968+Doc1325@users.noreply.github.com>
2024-03-16 19:36:44 -0400

    changes in main endpoint

--- Main.jsx
+++ Main.jsx
@@ -1,41 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import { SearchPage } from './src/Pages/SearchPage'
-import { MoviePage } from './src/Pages/MoviePage'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom'
-import { HomePage } from './src/Pages/HomePage'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage key='all' type='all' /> // las key me permiten que react identifiquen que son elementos distintos aun siendo el mismo componente
-
-  },
-  {
-    path: '/movies',
-    element: <HomePage key='movie' type='movie' />
-
-  },
-  {
-    path: '/tv',
-    element: <HomePage key='tv' type='tv' />
-
-  },
-  {
-    path: '/:media/:selectedMovie',
-    element: <MoviePage />
-  },
-  {
-    path: '/search/:search/:sort?',
-    element: <SearchPage />
-  }
-
-])
+import { App } from './src/App'
 const root = createRoot(document.getElementById('app'))
 root.render(
   <>
-    <RouterProvider router={router} />
+    <App />
   </>
 )
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import { SearchPage } from './Pages/SearchPage'
+import { MoviePage } from './Pages/MoviePage'
 import { HomePage } from './Pages/HomePage'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage key='all' type='all' /> // las key me permiten que react identifiquen que son elementos distintos aun siendo el mismo componente
+
+  },
+  {
+    path: '/movies',
+    element: <HomePage key='movie' type='movie' />
+
+  },
+  {
+    path: '/tv',
+    element: <HomePage key='tv' type='tv' />
+
+  },
+  {
+    path: '/:media/:selectedMovie',
+    element: <MoviePage />
+  },
+  {
+    path: '/search/:search/:sort?',
+    element: <SearchPage />
+  }
+
+])
 export function App () {
   return (
     <>
-      <HomePage type='all' />
+      <RouterProvider router={router} />
 
     </>
 
